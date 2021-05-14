0fda356392c4b34e1800d3c2194609b40ce52048
lisarothmuller <lisarothmuller@localhost>
2021-05-14 09:02:40 -0700

    includes work for the past few weeks. currently working on virus pathfinding as well

--- src/Virus.java
+++ src/Virus.java
@@ -1,4 +1,22 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
 
-public class Virus extends Bat{
-
+public class Virus {
+	private int posX,posY;
+	private int vx,vy;
+	
+	
+	
+	public Virus(int startingX, int startingY, int speed) {
+		
+		posX = startingX;
+		posY = startingY;
+		vx = speed;
+		vy = speed;
+		
+	}
 }
---
+++ src/buttons.java
@@ -0,0 +1,53 @@
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class buttons {
+	private int x = 0,y = 0;
+	private Image img; // image of the frog
+	private AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
+
+	public buttons(int initX, int initY, int buttonType) {
+		if (buttonType == 4) {
+		img = getImage("pixil-frame-0 (4).png");
+		} 
+			if (buttonType == 5) {
+				img = getImage("pixil-frame-0 (5).png");
+				
+		}if (buttonType == 3) {
+			img = getImage("pixil-frame-0 (3).png");
+			}
+		x = initX;
+		y = initY;
+		init(x,y);
+	}
+	
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(img, tx, null);
+		
+	}
+	
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(1.11, 1.1);
+	}
+
+
+	// converts image to make it drawable in paint
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = buttons.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+}
